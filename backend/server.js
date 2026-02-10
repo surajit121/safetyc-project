d220dee442b739ede7168cba9419e6ddb5d0a3cc
unknown <surajitkarmakar171@gmail.com>
2026-02-10 01:01:24 -0500

    Fix cross-domain session cookie issues with SameSite=None

--- backend/server.js
+++ backend/server.js
@@ -128,6 +128,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 24 * 60 * 60 * 1000 // 24 hours
     }
   })
